9d16f4d806a70ff3b6870c8935c3da0c67b9c605
Ricardo <aizekrysler@gmail.com>
2020-07-08 10:06:17 +0800

    add end command on sql

--- db/rename_cols_auth_user_table.sql
+++ db/rename_cols_auth_user_table.sql
@@ -1,2 +1,2 @@
-ALTER TABLE auth_users_tbl CHANGE auth_username username varchar(250)
-ALTER TABLE auth_users_tbl CHANGE auth_password password varchar(250)
+ALTER TABLE auth_users_tbl CHANGE auth_username username varchar(250);
+ALTER TABLE auth_users_tbl CHANGE auth_password password varchar(250);
