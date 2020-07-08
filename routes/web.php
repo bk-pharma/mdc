fb26afa938efa3e957212e4b970d46db25fee950
Jhay <romeo.marquez25@gmail.com>
2020-07-08 09:40:50 +0800

    working-login

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,12 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('layouts.admin');
 });
+
+
+Route::get('/test', function () {
+    $data = App\User::all();
+    return json_encode($data);
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
