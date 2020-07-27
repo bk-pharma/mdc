c5ddaf4485fdc720c95eeb3e46243741c49cd9d8
unknown <romeo.marquez25@gmail.com>
2020-07-27 16:22:42 +0800

    updating-dashboard

--- resources/js/sanitation/phaseOne.js
+++ resources/js/sanitation/phaseOne.js
@@ -1,6 +1,6 @@
-
 const BASE_URL = "http://server001.bell-kenz.com/mdc-new/mdc/public";
 const SUB_PHASE_URL = "http://server001.bell-kenz.com/mdc-new/mdc/public/sanitation/phase-two";
+
 new Vue({
   el: '#sanitationPhaseOne-container',
   data() {
