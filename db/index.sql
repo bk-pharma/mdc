75adf68febf294ca2b19028057d7f7cf6c1e2951
Ricardo <aizekrysler@gmail.com>
2020-08-06 14:38:48 +0800

    added important config for db tuning

---
+++ db/index.sql
@@ -0,0 +1,8 @@
+CREATE INDEX rawData ON sanitation_result_new (raw_status, raw_corrected_name, raw_date);
+CREATE INDEX rawData ON sanitation_result_new (raw_id);
+CREATE INDEX mdName ON db_sanitation (sanit_mdname);
+CREATE INDEX mdName ON db_sanitation2 (sanit_surname, sanit_firstname, sanit_middlename);
+CREATE INDEX mdName ON db_sanitation3 (sanit_name, sanit_license);
+CREATE INDEX mdName ON db_sanitation4 (sanit_surname, sanit_firstname, sanit_middlename, sanit_branch);
+CREATE INDEX mdName ON rules_details (details_column_name, details_value, rule_code);
+CREATE INDEX mdName ON rules_tbl (rule_code);
