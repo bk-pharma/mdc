53d0eee6e3cd7bfccc17325e0db9bc709c38bd7e
Ricardo <aizekrysler@gmail.com>
2020-08-09 16:54:41 +0800

    call sanitizedCount on load

--- resources/js/sanitation/automated.js
+++ resources/js/sanitation/automated.js
@@ -10,64 +10,68 @@ new Vue({
            totalSanitizedAmount: 0
         }
     },
-
-methods : {
-    startConsole: function()
-    {
-        this.automatedLabel = 'Starting to Sanitize all the data . . . .';
-
-        let rowsPerSanitationProcess = 50;
-        let sanitationProcessNeeded = (this.rowCount / rowsPerSanitationProcess);
-        let processRowStart = 0;
-
-        for(let i = 0; i < sanitationProcessNeeded; i++)
+    created() {
+        this.sanitizedCount();
+    },
+    methods : {
+        startConsole: function()
         {
-            if(i === 0)
+            this.automatedLabel = 'Starting to Sanitize all the data . . . .';
+
+            let rowsPerSanitationProcess = 50;
+            let sanitationProcessNeeded = (this.rowCount / rowsPerSanitationProcess);
+            let processRowStart = 0;
+
+            for(let i = 0; i < sanitationProcessNeeded; i++)
             {
-                processRowStart = 0;
-            }else
-            {
-                processRowStart = (i * rowsPerSanitationProcess) + 1;
+                if(i === 0)
+                {
+                    processRowStart = 0;
+                }else
+                {
+                    processRowStart = (i * rowsPerSanitationProcess) + 1;
+                }
+
+                this.processRowStartArr.push(processRowStart);
             }
 
-            this.processRowStartArr.push(processRowStart);
+            this.sanitationProcess(0, this.processRowStartArr[0], rowsPerSanitationProcess);
+        },
+        sanitationProcess: function(index, rowStart, rowCount)
+        {
+            let totalProcess = this.processRowStartArr.length;
+
+            axios.get(`automated/start-process/${rowStart}/${rowCount}`)
+            .then((response) =>
+            {
+                this.sanitizedCount();
+
+                let nextRowStart = this.processRowStartArr[index + 1];
+                let nextIndex = index + 1;
+
+                if(typeof nextRowStart !== 'undefined')
+                {
+                    this.sanitationProcess(nextIndex, nextRowStart, rowCount);
+                }
+            })
+            .catch((error) =>
+            {
+                console.log(error);
+            })
+        },
+        sanitizedCount: function()
+        {
+            axios.get(`automated/sanitized-total`)
+            .then((response) => {
+
+                let resp = response.data;
+                console.log(resp);
+
+            })
+            .catch((error) => {
+
+            })
         }
-
-        this.sanitationProcess(0, this.processRowStartArr[0], rowsPerSanitationProcess);
-    },
-    sanitationProcess: function(index, rowStart, rowCount)
-    {
-        let totalProcess = this.processRowStartArr.length;
-
-        axios.get(`automated/start-process/${rowStart}/${rowCount}`)
-        .then((response) =>
-        {
-            let nextRowStart = this.processRowStartArr[index + 1];
-            let nextIndex = index + 1;
-
-            if(typeof nextRowStart !== 'undefined')
-            {
-                this.sanitationProcess(nextIndex, nextRowStart, rowCount);
-            }
-        })
-        .catch((error) =>
-        {
-            console.log(error);
-        })
-    },
-    sanitizedCount: function()
-    {
-        axios.get(`automated/sanitized-total`)
-        .then((response) => {
-
-            let resp = response.data;
-            console.log(resp);
-
-        })
-        .catch((error) => {
-
-        })
     }
-}
 
 });
