ef34affecd0f0cd82c40e5d8711d5b840dd98b90
Ricardo <aizekrysler@gmail.com>
2020-09-01 13:37:53 +0800

    chunk size and batch size to 1k

--- app/Imports/RawDataImport.php
+++ app/Imports/RawDataImport.php
@@ -129,12 +129,12 @@ class RawDataImport implements ToModel, WithHeadingRow, WithChunkReading, WithBa
 
   public function chunkSize(): int
   {
-      return 2000;
+      return 1000;
   }
 
   public function batchSize(): int
   {
-      return 2000;
+      return 1000;
   }
 
   private function getAmountPerTab($rawData)
