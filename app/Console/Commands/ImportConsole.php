a044e374e815eace97c76c8965e3941baedc4aa3
Ricardo <aizekrysler@gmail.com>
2020-09-03 18:52:17 +0800

    use laravel excel facades

--- app/Console/Commands/ImportConsole.php
+++ app/Console/Commands/ImportConsole.php
@@ -6,7 +6,7 @@ use App\Services\Contracts\RawDataInterface;
 use App\Imports\RawDataImport;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
-use Rap2hpoutre\FastExcel\FastExcel;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ImportConsole extends Command
 {
@@ -22,35 +22,12 @@ class ImportConsole extends Command
     	$this->raw_data = $raw_data;
     }
 
-  private function getAmountPerTab($rawData)
-  {
-    if($rawData['qty'] == 0)
-    {
-      return 0;
-    }else
-    {
-      return $rawData['amount'] / $rawData['qty'];
-    }
-  }
-
-  private function getAmountPerPack($amountPerTab, $rawData, $product)
-  {
-    if($amountPerTab <= ($product->prod_pertab + 1))
-    {
-      return $rawData['qty'] / $product->prod_packsize;
-    }else
-    {
-      return $rawData['qty'];
-    }
-  }
-
     public function handle()
     {
     	$fileName = $this->option('file_name');
 
-
         $this->output->title('Starting import');
-       	$this->excel->import(new RawDataImport($this->raw_data), storage_path('app/uploads/rawData/'.$fileName));
+       	Excel::import(new RawDataImport($this->raw_data), storage_path('app/uploads/rawData/'.$fileName));
         unlink(storage_path('app/uploads/rawData/'.$fileName));
         $this->output->success('Import successful');
     }
